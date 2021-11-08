c5c7a61e31c5438dfd4d5c82c76b59f84554a28a
Eugene <fakemail@fbgrtraqqrjjsaabvcbcvb2.com>
2021-11-08 04:21:26 +0300

    latest

--- djApi/lesson3.py
+++ djApi/lesson3.py
@@ -1,22 +1,45 @@
 import psycopg2
 
-conn = psycopg2.connect(dbname='practice_db', user='eugene',
+conn = psycopg2.connect(dbname='django_pg', user='eugene',
                         password='123', host='localhost')
 
 cursor = conn.cursor()
 
+# SQL = """
+#     CREATE TABLE less1
+#     (
+#         Id SERIAL PRIMARY KEY,
+#         FirstName VARCHAR(30),
+#         LastName VARCHAR(30),
+#         Email VARCHAR(30),
+#         Age INTEGER
+#     )
+# """
+
+# SQL = """
+#     INSERT INTO product (product_name,
+#             company_name,
+#             description,
+#             price)
+#
+#         VALUES ('TT',
+#             'Audi',
+#             'автомобиль',
+#             35000);
+# """
+
+# SQL = """
+#     SELECT * FROM main_auto WHERE brand_id IN (SELECT id FROM main_brand WHERE name IN ('Audi', 'BMW'));
+# """
+
 SQL = """
-    CREATE TABLE less1
-    (
-        Id SERIAL PRIMARY KEY,
-        FirstName VARCHAR(30),
-        LastName VARCHAR(30),
-        Email VARCHAR(30),
-        Age INTEGER
-    )
+    SELECT * FROM main_auto WHERE brand_id IN (SELECT id FROM main_brand WHERE name <> 'Audi'); 
 """
 
 cursor.execute(SQL)
+
+print(cursor.fetchall())
+
 
 conn.commit()
 conn.close()
--- djApi/main/views.py
+++ djApi/main/views.py
@@ -3,8 +3,9 @@ from .models import Auto, Brand, Detail
 
 
 def home_page(request):
-    auto = Auto.objects.filter()
-    print(Auto.objects)
-    print(dir(Auto.objects))
-    context = {}
+    auto = Auto.objects.filter(pk=1)
+    print(auto.query)
+    print(auto[0].brand)
+    # print(dir(Auto.objects))
+    context = {'auto': auto}
     return render(request, 'main/index.html', context)
